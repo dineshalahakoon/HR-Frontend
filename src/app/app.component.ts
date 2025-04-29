fcf24c404f5a4f0f929378da3922243d66340737
dineshalahakoon <dineshroxx1997@gmail.com>
2025-04-29 11:56:48 +0530

    pages folder make

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './common/nav/nav.component';
-import { AddEmployeeComponent } from "./add-employee/add-employee.component";
+import { AddEmployeeComponent } from "./pages/add-employee/add-employee.component";
 
 @Component({
   selector: 'app-root',
